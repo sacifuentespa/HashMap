f74723e625b64a93c7147c55519317a20c7a0d75
sacifuentespa <85040512+sacifuentespa@users.noreply.github.com>
2024-04-01 23:31:05 -0500

    set implementation; added findEmptyOrMatchingIndex

--- hashMap.js
+++ hashMap.js
@@ -5,12 +5,6 @@ class HashMap {
         this.LOADFACTOR = 0.75;
     }
 
-    checkLimit(index){
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-          }
-    }
-
     hash(key) {
         let hashCode = 0;
         key = key.toString();
@@ -20,14 +14,23 @@ class HashMap {
         return hashCode;
     }
 
-    set(key, value){
+    set(key, value) {
         let hashKey = this.hash(key);
-        if(this.buckets[hashKey]  === undefined){
-            this.buckets[hashKey] = {key,value}
-        } else if(this.buckets[hashKey].key === key){
-            this.buckets[hashKey].value = value;
-        }
+        let index = this.findEmptyOrMatchingIndex(hashKey, key);
+        this.buckets[index] = { key, value };
     }
+
+    findEmptyOrMatchingIndex(startIndex, key) {
+        let index = startIndex;
+        do {
+            if (this.buckets[index] === undefined || this.buckets[index].key === key) {
+                return index;
+            }
+            index = (index + 1) % this.buckets.length;
+        } while (index !== startIndex);
+        throw new Error('HashMap is full');
+    }
+
 
     get getBuckets() {
         console.log(this.buckets)
@@ -36,16 +39,23 @@ class HashMap {
 
 let firstHash = new HashMap;
 
-/*console.log(firstHash.hash("rafa"));
-console.log(firstHash.hash("fara"));
-console.log(firstHash.hash("rafa"));
-console.log(firstHash.hash("fara"));
+/*console.log(firstHash.hash(23));
+
+console.log(firstHash.hash(13));
+console.log(firstHash.hash(33));
+console.log(firstHash.hash(14));
 */
 
 
-console.log(firstHash.set(23));
+
+console.log(firstHash.set(23, 23));
 console.log(firstHash.getBuckets);
 console.log(firstHash.buckets[13]);
-console.log(firstHash.set(23,56));
+console.log(firstHash.set(23, 56));
 console.log(firstHash.getBuckets);
 console.log(firstHash.buckets[13]);
+
+console.log(firstHash.set(13, 96));
+console.log(firstHash.set(13, 46));
+console.log(firstHash.set(33, 498));
+console.log(firstHash.getBuckets);
