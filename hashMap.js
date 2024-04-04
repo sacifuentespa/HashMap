5f639876725edd138a38bb24557871a2ca4f50b4
sacifuentespa <85040512+sacifuentespa@users.noreply.github.com>
2024-04-04 10:30:39 -0500

    fixed bug when rehashing values when the buckets length changed unintentionally

--- hashMap.js
+++ hashMap.js
@@ -11,24 +11,51 @@ class HashMap {
         key = key.toString();
         for (let i = 0; i < key.length; i++) {
             hashCode = (key.charCodeAt(i) * i * this.PRIMENUMBER + hashCode) % this.buckets.length;
+            console.log(this.buckets.length);
         }
         return hashCode;
     }
 
     set(key, value) {
-         
-        if (this.findMatchingIndex(key) === -1){
+
+        if (this.findMatchingIndex(key) === -1) {
             this.size++
-        } 
+        }
         let index = this.findEmptyOrMatchingIndex(key);
         this.buckets[index] = { key, value };
+        console.log(`key: ${key} hash:${index}`);
+
+        // Check if resizing is needed
+        if (this.length() > this.buckets.length * this.LOADFACTOR) {
+            // Create a new array with double the size
+            const newSize = this.buckets.length * 2;
+            const newBuckets = new Array(newSize);
+            const tempBuckets = this.buckets;
+            this.buckets = newBuckets;
+
+            // Rehash and reinsert each key-value pair
+            for (let i = 0; i < tempBuckets.length; i++) {
+                if (tempBuckets[i] !== undefined) {
+                    let { key, value } = tempBuckets[i];
+                    let newIndex = this.hash(key);
+                    console.log(`key: ${key} hash:${newIndex}`);
+                    while (this.buckets[newIndex] !== undefined) {
+                        if (newIndex == this.buckets.length-1) {
+                            newIndex = 0
+                        }
+                        else { newIndex = newIndex + 1 }; // Handle collisions
+                    }
+                    this.buckets[newIndex] = { key, value };
+                }
+            }
+        }
     }
 
     get(key) {
 
-    // takes one argument as a key and returns the value that
-    // is assigned to this key. 
-    // If a key is not found, return null.
+        // takes one argument as a key and returns the value that
+        // is assigned to this key. 
+        // If a key is not found, return null.
 
         let index = this.findMatchingIndex(key);
         return this.buckets[index] ? this.buckets[index].value : null;
@@ -80,47 +107,47 @@ class HashMap {
         return -1;
     }
 
-    length(){
+    length() {
         // returns the number of stored keys in the hash map. 
-        
+
         return this.size;
     }
 
-    clear(){
+    clear() {
 
         //removes all entries in the hash map.
 
-        for(let i = 0; i < this.buckets.length; i++){
+        for (let i = 0; i < this.buckets.length; i++) {
             this.buckets[i] = undefined;
         }
     }
 
-    keys(){
+    keys() {
         // returns an array containing all the keys inside the hash map.
         let keysArray = []
-        for(let i = 0; i < this.buckets.length; i++){
-            if (this.buckets[i] !== undefined){
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] !== undefined) {
                 keysArray.push(this.buckets[i].key)
             }
         }
         return keysArray;
     }
 
-    values(){
+    values() {
         // returns an array containing all the keys inside the hash map.
         let valuesArray = []
-        for(let i = 0; i < this.buckets.length; i++){
-            if (this.buckets[i] !== undefined){
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] !== undefined) {
                 valuesArray.push(this.buckets[i].value)
             }
         }
         return valuesArray;
     }
 
-    entries(){
+    entries() {
         let entriesArray = []
-        for(let i = 0; i < this.buckets.length; i++){
-            if (this.buckets[i] !== undefined){
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] !== undefined) {
                 entriesArray.push(this.buckets[i])
             }
         }
@@ -144,28 +171,37 @@ console.log(firstHash.hash(14));
 
 
 console.log(firstHash.set(23, 23));
-console.log(firstHash.getBuckets);
-console.log(firstHash.buckets[13]);
+
+
 console.log(firstHash.set(23, 56));
-console.log(firstHash.getBuckets);
-console.log(firstHash.buckets[13]);
+
+
 
 console.log(firstHash.set(13, 96));
+
 console.log(firstHash.set(13, 46));
+
 console.log(firstHash.set(33, 498));
 
+console.log(firstHash.set(43, 498));
+console.log(firstHash.set(53, 498));
+console.log(firstHash.set(63, 4));
+console.log(firstHash.set(73, 498));
+
+console.log(firstHash.set(83, 498));
+console.log(firstHash.set(93, 498));
+console.log(firstHash.set(103, 498));
+
+console.log(firstHash.set(203, 498));
+console.log(firstHash.set(303, 498));
+console.log(firstHash.getBuckets);
+console.log(firstHash.set(403, 498));
+console.log(firstHash.getBuckets);
+
+console.log(firstHash.set(93, 48186));
+
+console.log(firstHash.hash(93));
+console.log(firstHash.hash(83));
+console.log(firstHash.hash(84));
 
 console.log(firstHash.getBuckets);
-console.log(firstHash.get(33));
-console.log(firstHash.has(34));
-console.log(firstHash.has(33));
-
-console.log(firstHash.length());
-console.log(firstHash.keys());
-console.log(firstHash.remove(33));
-console.log(firstHash.getBuckets);
-console.log(firstHash.get(33));
-console.log(firstHash.length());
-console.log(firstHash.keys());
-console.log(firstHash.values());
-console.log(firstHash.entries());
